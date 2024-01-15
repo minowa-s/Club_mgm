95ddcabed7fa2e5e4435e096c9f940574ddbf6ed
hitoki <h.nakamura.sys22@morijyobi.ac.jp>
2024-01-15 09:46:28 +0900

    ログイン作成中

--- db.py
+++ db.py
@@ -1,12 +1,141 @@
+import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import hashlib
 import os, psycopg2
+import string
+import random
 import smtplib
 
 def get_connection():
     url = os.environ['DATABASE_URL']
     connection = psycopg2.connect(url)
     return connection
+
+#ハッシュ取得
+# def get_hash(password, salt):
+#     b_pw = bytes(password, 'utf-8')
+#     b_salt = bytes(salt, 'utf-8')
+#     hashed_password = hashlib.pbkdf2_hmac('sha256', b_pw, b_salt, 200).hex()
+#     return hashed_password
+    #------------
+def get_hash(password, salt):
+    # 文字列をバイト列に変換
+    b_password = bytes(password, 'utf-8')
+    b_salt = bytes(salt, 'utf-8')
+    # PBKDF2_HMAC イテレーション数: 200, ハッシュアルゴリズム: SHA-256
+    hashed_password = hashlib.pbkdf2_hmac('sha256', b_password, b_salt, 200).hex()
+    return hashed_password
+#------------
+
+# ワンタイムパスワード取得
+def select_otp(mail):
+    sql = "SELECT onetimepassword from student WHERE mail = %s"
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (mail,))
+        passw = cursor.fetchone() 
+        
+    except psycopg2.DatabaseError:
+        flg = False
+    finally:
+        cursor.close()
+        connection.close()
+        
+    return passw
+
+#アカウントパスワード取得
+def get_account_pass(mail):
+    sql = 'SELECT password FROM users WHERE mail = %s'
+    
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (mail,))
+        passw = cursor.fetchone()
+        str_pw = str(passw[0])   
+        
+    except psycopg2.DatabaseError:
+        flg = False
+    finally:
+        cursor.close()
+        connection.close()
+    return str_pw
+
+#アカウントソルト取得
+def get_account_salt(mail):
+    sql = 'SELECT salt FROM users WHERE mail = %s'
+    
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (mail,))
+        salt = cursor.fetchone()
+        str_salt = str(salt[0])
+    except psycopg2.DatabaseError:
+        flg = False
+    finally:
+        cursor.close()
+        connection.close()  
+    return str_salt
+
+#ソルト生成
+def get_salt():
+    charset = string.ascii_letters + string.digits
+    salt = '' .join(random.choices(charset, k=30))
+    return salt
+
+#パスワード生成(教員初期ぱすわーど)
+def generate_pass():
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(8))
+    return random_string
+        
+# ワンタイムパスワード取得    
+def generate_otp():
+    # 6桁のランダムな数字を生成
+    otp = ''.join([str(random.randint(0, 9)) for _ in range(6)])
+    return otp
+
+# 学科取得（登録時に選択でだすやつ）
+def select_department():
+    sql = "SELECT * FROM department"
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    department_list = []
+    for row in cursor.fetchall():
+        department_list.append((row[0], row[1]))
+    connection.close()
+    return department_list
+
+#入学年度選択
+def select_year():
+    current_year = datetime.datetime.now().year
+    year_range = range(current_year, current_year - 4, -1)
+    return year_range
+
+#メール送信 
+def send_email(to_address, subject, body):
+    # 送信元と送信先のメールアドレス
+    from_address = "h.nakamura.sys22@morijyobi.ac.jp"
+    app_password = "lydt vxfw inil lffe"  # Gmailのアプリパスワードなどを使用してください
+
+    # メールの設定
+    msg = MIMEMultipart()
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Subject'] = subject
+
+    # メール本文
+    msg.attach(MIMEText(body, 'plain'))
+
+    # SMTPサーバーに接続してメールを送信
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(from_address, app_password)
+        server.sendmail(from_address, to_address, msg.as_string())
 
 #サークル立ち上げ申請
 def request_club(club_name, leader_mail, objective, activities, introduction, note):
