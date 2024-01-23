181ea14709ff5112c475de8f0c2e60c61f00a3ab
h.takahashi <h.takahashi.sys22@morijyobi.ac.jp>
2024-01-23 12:41:32 +0900

    修正

--- app.py
+++ app.py
@@ -36,15 +36,26 @@ app.register_blueprint(app_req_bp)
 
 #------------------------------------
 
+#DB接続
+def get_connection():
+    connection = psycopg2.connect(
+        host = 'ec2-54-234-13-16.compute-1.amazonaws.com',
+        port = 5432,
+        user = 'pcdytjuqlssmhk',
+        database = 'de5b43sf8b9ocm',
+        password = '736926275fcb36df9c9fc0255fe0cb285a3155d3c91a6bc3aeb6a67dbb505869'
+    )
+    return connection
+
 @app.route('/')
 def index():
-    return render_template('top/top.html')
+    club_list = club.club_list()
+    return render_template('top/top.html', club_list=club_list)
 
 @app.route('/backtop_s')
 def backtop_student():
     club_list = club.club_list()
     return render_template('top/top_student.html', club_list=club_list)
-
 
 @app.route('/backtop_t')
 def backtop_teacher():
