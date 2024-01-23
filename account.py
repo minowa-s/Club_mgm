958e5b480ecb077c34db9006bea5cfb91f91921f
hitoki <h.nakamura.sys22@morijyobi.ac.jp>
2024-01-23 12:28:02 +0900

    a

--- account.py
+++ account.py
@@ -1,6 +1,3 @@
-
-
-
 from flask import Blueprint, redirect, render_template, request, session, url_for
 import hashlib, string, random, psycopg2, db, os, bcrypt, datetime, smtplib, club
 from email.mime.text import MIMEText
