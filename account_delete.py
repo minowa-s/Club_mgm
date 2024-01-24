0ee5eada5a76ff2488ae5d09a462e807b07ed4e1
h.takahashi <h.takahashi.sys22@morijyobi.ac.jp>
2024-01-24 11:43:02 +0900

    DB変更

--- account_delete.py
+++ account_delete.py
@@ -8,8 +8,13 @@ account_delete_bp = Blueprint('account_delete', __name__, url_prefix='/account_d
 
 #DB接続
 def get_connection():
-    url = os.environ['DATABASE_URL']
-    connection = psycopg2.connect(url)
+    connection = psycopg2.connect(
+        host = 'ec2-54-205-67-130.compute-1.amazonaws.com',
+        port = 5432,
+        user = 'hpmpphajmsgupf',
+        database = 'def063tgglhgba',
+        password = 'e8d1d1bce580c7529b0a61b91773c3b407b77ac49ba0a91ac20388370e7b3c47'
+    )
     return connection
 
 @account_delete_bp.route('/account_delete', methods = ["POST"])
