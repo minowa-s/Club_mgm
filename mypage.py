eca82ccc4e42bcfb03c6fcd41d708621a1919e00
h.takahashi <h.takahashi.sys22@morijyobi.ac.jp>
2024-01-24 11:42:29 +0900

    DB変更

--- mypage.py
+++ mypage.py
@@ -8,8 +8,13 @@ mypage_bp = Blueprint('mypage_bp', __name__, url_prefix='/mypage_bp')
 
 #DB接続
 def get_connection():
-    url = os.environ['DATABASE_URL']
-    connection = psycopg2.connect(url)
+    connection = psycopg2.connect(
+        host = 'ec2-54-205-67-130.compute-1.amazonaws.com',
+        port = 5432,
+        user = 'hpmpphajmsgupf',
+        database = 'def063tgglhgba',
+        password = 'e8d1d1bce580c7529b0a61b91773c3b407b77ac49ba0a91ac20388370e7b3c47'
+    )
     return connection
 
 #マイページ機能
